ff961e2f1c4f87e4bb074bd9df38688a482f630e
nguetcheu <dnguetcheu@gmail.com>
2023-07-20 02:44:43 +0200

    Ajout des annotations

--- spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/repository/UserRepository.java
+++ spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.nguetcheSpring.springsecurityclient.repository;
 
 import com.nguetcheSpring.springsecurityclient.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/service/UserServiceImpl.java
+++ spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/service/UserServiceImpl.java
@@ -1,4 +1,7 @@
 package com.nguetcheSpring.springsecurityclient.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService{
 }
