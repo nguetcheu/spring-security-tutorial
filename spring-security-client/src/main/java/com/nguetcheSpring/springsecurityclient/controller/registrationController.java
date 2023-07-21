66b9346462a5771503499dbad6bb2e0cebb3317d
nguetcheu <dnguetcheu@gmail.com>
2023-07-21 02:28:21 +0200

    Mise en place de environnement exécution du code de Spring security

--- spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/controller/registrationController.java
+++ spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/controller/registrationController.java
@@ -1,9 +1,11 @@
 package com.nguetcheSpring.springsecurityclient.controller;
 
 import com.nguetcheSpring.springsecurityclient.entity.User;
+import com.nguetcheSpring.springsecurityclient.event.RegistrationCompleteEvent;
 import com.nguetcheSpring.springsecurityclient.model.UserModel;
 import com.nguetcheSpring.springsecurityclient.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,15 @@ public class registrationController  {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @PostMapping("/register")
     public String registerUser(@RequestBody UserModel userModel) {
         User user = userService.registerUser(userModel);
+        publisher.publishEvent(new RegistrationCompleteEvent(
+                user,"url"
+        ));
         return "Success";
     }
 }
---
+++ spring-security-client/src/main/java/com/nguetcheSpring/springsecurityclient/event/RegistrationCompleteEvent.java
@@ -0,0 +1,16 @@
+package com.nguetcheSpring.springsecurityclient.event;
+
+import com.nguetcheSpring.springsecurityclient.entity.User;
+import org.springframework.context.ApplicationEvent;
+
+public class RegistrationCompleteEvent extends ApplicationEvent {
+
+    private User user;
+    private String applicationUrl;
+
+    public RegistrationCompleteEvent(User user, String applicationUrl) {
+        super(user);
+        this.user = user;
+        this.applicationUrl = applicationUrl;
+    }
+}
